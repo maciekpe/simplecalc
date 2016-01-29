ac7bac087c9869e2c888d054308c03c2c248973c
Maciej Peruga <maciej.peruga@4finance.com>
2016-01-29 17:42:53 +0100

    actions handling

--- simplecalc/simplecalc/ViewController.m
+++ simplecalc/simplecalc/ViewController.m
@@ -10,6 +10,28 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *resultLabel;
+@property (weak, nonatomic) IBOutlet UIButton *acBtn;
+@property (weak, nonatomic) IBOutlet UIButton *plusMinusBtn;
+@property (weak, nonatomic) IBOutlet UIButton *percentageBtn;
+@property (weak, nonatomic) IBOutlet UIButton *divBtn;
+@property (weak, nonatomic) IBOutlet UIButton *multiBtn;
+@property (weak, nonatomic) IBOutlet UIButton *plusBtn;
+@property (weak, nonatomic) IBOutlet UIButton *minusBtn;
+@property (weak, nonatomic) IBOutlet UIButton *calculateBtn;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *oneBtn;
+@property (weak, nonatomic) IBOutlet UIButton *twoBtn;
+@property (weak, nonatomic) IBOutlet UIButton *threeBtn;
+@property (weak, nonatomic) IBOutlet UIButton *fourBtn;
+@property (weak, nonatomic) IBOutlet UIButton *fiveBtn;
+@property (weak, nonatomic) IBOutlet UIButton *sixBtn;
+@property (weak, nonatomic) IBOutlet UIButton *sevenBtn;
+@property (weak, nonatomic) IBOutlet UIButton *eigthBtn;
+@property (weak, nonatomic) IBOutlet UIButton *nineBtn;
+@property (weak, nonatomic) IBOutlet UIButton *zeroBtn;
+
 @end
 
 @implementation ViewController
@@ -23,5 +45,51 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)oneTappedAct:(id)sender {
+}
+
+- (IBAction)twoTappedAct:(id)sender {
+}
+
+- (IBAction)threeTappedAct:(id)sender {
+}
+
+- (IBAction)fourTappedAct:(id)sender {
+}
+
+- (IBAction)fiveTappedAct:(id)sender {
+}
+
+- (IBAction)sixTappedAct:(id)sender {
+}
+
+- (IBAction)sevenTappedAct:(id)sender {
+}
+
+- (IBAction)eightTappedAct:(id)sender {
+}
+
+- (IBAction)nineTappedAct:(id)sender {
+}
+
+- (IBAction)zeroTappedAct:(id)sender {
+}
+
+- (IBAction)divTappedAct:(id)sender {
+}
+
+- (IBAction)multiTappedAct:(id)sender {
+}
+
+- (IBAction)plusTappedAct:(id)sender {
+}
+
+- (IBAction)minusTappedAct:(id)sender {
+}
+
+- (IBAction)calculateTappedAct:(id)sender {
+}
+
 
 @end
